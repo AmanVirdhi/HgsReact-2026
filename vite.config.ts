c03e57edaac168432976fbf32f7017679c9ff70d
AMANDEEP VIRDHI <amanvirdhi123@gmail.com>
2026-04-30 21:19:31 +0530

    Merge pull request #4 from AmanVirdhi/fix/github-route1
    fix:build folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/HgsReact-2026/',
+  build: {
+    outDir: 'build'
+  },
   server: {
     port: 3000,
     open: true
