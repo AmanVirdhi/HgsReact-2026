75d867b787a0d48fc4054065b479b7690f52c2d3
AMANDEEP VIRDHI <amanvirdhi123@gmail.com>
2026-05-04 01:16:35 +0530

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from './components/Login/Login';
 import { Home } from './components/Home/Home';
 import { HgsTypes } from './components/HgsTypes/HgsTypes';
